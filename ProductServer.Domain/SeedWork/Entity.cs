2c05c5fde1672de51ab69e56dba2e0831a5edfb3
Federico Jose Lucia <federicojoselucia@gmail.com>
2022-03-16 14:13:09 -0300

    Domain layer seedwork

---
+++ ProductServer.Domain/SeedWork/Entity.cs
@@ -0,0 +1,93 @@
+﻿using MediatR;
+
+namespace ProductServer.Domain.SeedWork
+{
+    public abstract class Entity<TId>
+    {
+        int? _requestedHashCode;
+        TId? _Id;
+        private readonly List<INotification> domainEvents;
+
+        public virtual TId? Id
+        {
+            get
+            {
+                return _Id;
+            }
+            protected set
+            {
+
+                _Id = value;
+            }
+        }
+
+        public Entity()
+        {
+            domainEvents = new List<INotification>();
+        }
+
+        public IReadOnlyList<INotification> DomainEvents => domainEvents.AsReadOnly();
+
+        protected void AddDomainEvent(INotification eventItem)
+        {
+            domainEvents.Add(eventItem);
+
+        }
+
+        public void RemoveDomainEvent(INotification eventItem)
+        {
+            if (domainEvents is null) return;
+            domainEvents.Remove(eventItem);
+        }
+
+        public void ClearDomainEvents()
+        {
+            domainEvents?.Clear();
+        }
+
+        public bool IsTransient()
+        {
+            return Object.Equals(Id, default(TId));
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj is not Entity<TId>)
+                return false;
+            if (Object.ReferenceEquals(this, obj))
+                return true;
+            if (this.GetType() != obj.GetType())
+                return false;
+            Entity<TId> item = (Entity<TId>)obj;
+            if (item.IsTransient() || this.IsTransient())
+                return false;
+            else
+                return Object.Equals(item.Id, Id);
+        }
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode = this.Id!.GetHashCode() ^ 31;
+                // XOR for random distribution. See:
+                // https://blogs.msdn.microsoft.com/ericlippert/2011/02/28/guidelines-and-rules-for-gethashcode/
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+        }
+        public static bool operator ==(Entity<TId> left, Entity<TId> right)
+        {
+            if (Object.Equals(left, null))
+                return (Object.Equals(right, null));
+            else
+                return left.Equals(right);
+        }
+        public static bool operator !=(Entity<TId> left, Entity<TId> right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ ProductServer.Domain/SeedWork/Enumeration.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+
+namespace ProductServer.Domain.SeedWork
+{
+    public abstract class Enumeration : IComparable, IEquatable<Enumeration>
+    {
+        public string Name { get; private set; }
+
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public |
+                                             BindingFlags.Static |
+                                             BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Enumeration otherValue)
+                return false;
+
+            var typeMatches = GetType() == obj.GetType();
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public int CompareTo(object? other) => Id.CompareTo(((Enumeration)other!).Id);
+
+        public bool Equals(Enumeration? other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Name == other.Name && Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Id);
+        }
+
+        public static bool operator ==(Enumeration? left, Enumeration? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Enumeration? left, Enumeration? right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
