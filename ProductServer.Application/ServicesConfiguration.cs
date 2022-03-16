604568d5e1d84a9e54ed4db26d33464d691af79a
Federico Jose Lucia <federicojoselucia@gmail.com>
2022-03-16 17:07:31 -0300

    Application seedwork and behaviours

---
+++ ProductServer.Application/ServicesConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductServer.Application.Behaviors;
+
+namespace ProductServer.Application
+{
+    public static class ServicesConfiguration
+    {
+        public static void AddApplicationLayer(this IServiceCollection services, IConfiguration _)
+        {
+            services.AddMediatR(typeof(ServicesConfiguration).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(StopwatchLoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CommandLoggingBehavior<,>));
+        }
+    }
+}
